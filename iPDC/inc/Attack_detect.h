b10a48516cb14c4d98ed19d6b81f40bc3e1123b4
pavanvpatil <pavanvpatil01@gmail.com>
2022-10-28 19:32:32 +0530

    AlGO DONE FOR GAUSSIAN

--- iPDC/inc/Attack_detect.h
+++ iPDC/inc/Attack_detect.h
@@ -1,8 +1,8 @@
 /* pavan changes */
-#include <time.h>
 #include <gtk/gtk.h>
 /* variables declared of attack_detect function */
 
 /* function declared  */
-gboolean attack_detect(struct data_frame *df);
+gboolean attack_detect_freq(struct data_frame *df);
+gboolean attack_detect_vol(struct data_frame *df)
 /* pavan changes */
